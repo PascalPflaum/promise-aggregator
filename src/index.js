10e2b37cfcb90399ccab21f098cc283363903a2b
Pascal Pflaum <mail@pascalpflaum.de>
2017-02-27 23:11:58 +0100

    eslint

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 const instanceFactory = (cb) => {
-  let runningPromise = null;
-  let aggregations = [];
+    let runningPromise = null;
+    let aggregations = [];
 
-  function buildCB (method) {
-    return function () {
-      aggregations.forEach((aggregation) => {
-        aggregation[method].apply(this, arguments);
-      });
-      runningPromise = null;
-      aggregations = [];
+    function buildCB(method) {
+        return function () {
+            aggregations.forEach((aggregation) => {
+                aggregation[method].apply(this, arguments);
+            });
+            runningPromise = null;
+            aggregations = [];
+        };
+    }
+
+    const onEnd = buildCB('resolve');
+    const onFail = buildCB('reject');
+
+    const newAggregation = () => {
+        return new Promise(function (resolve, reject) {
+            aggregations.push({
+                resolve,
+                reject,
+            });
+        });
     };
-  }
 
-  const onEnd = buildCB('resolve');
-  const onFail = buildCB('reject');
-
-  const newAggregation = () => {
-    return new Promise(function (resolve, reject) {
-      aggregations.push({
-        resolve,
-        reject,
-      });
-    });
-  };
-
-  function wrapper () {
-    if (!runningPromise) {
-      runningPromise = cb()
+    function wrapper() {
+        if (!runningPromise) {
+            runningPromise = cb()
                 .then(onEnd)
                 .catch(onFail);
+        }
+        return newAggregation();
     }
-    return newAggregation();
-  }
 
-  return wrapper;
+    return wrapper;
 };
 
 module.exports = instanceFactory;
